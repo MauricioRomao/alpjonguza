1366cb419903b525df7e9147daf81a34cb84af70
MauricioRomao <devmauroromao@gmail.com>
2024-08-15 01:23:52 +0100

    responsividade

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ window.addEventListener('scroll', function() {
     var menu = document.querySelector('.header_menu');
     var menu_afastado = document.querySelector(".menu_afastado a");
 
+
     if (window.scrollY > header.clientHeight) {
         menu.classList.add('menu_scroll');
         menu_afastado.classList.add("menu_scroll");
@@ -15,10 +16,14 @@ window.addEventListener('scroll', function() {
 
 // - Menu escndido
 let menu_escondido = document.querySelector(".menu_escondido")
+const esc_mennu = document.querySelectorAll(".escoder")
 let bx_menu = document.querySelector(".bx-menu")
+
 bx_menu.addEventListener("click", function(){
     menu_escondido.classList.toggle("menu_ativo")
     bx_menu.classList.toggle("bx_color")
 
     
 })
+
+
