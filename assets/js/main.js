d76e3ab4c66e3ffb6d2cfb00077032d549d4aac6
MauricioRomao <devmauroromao@gmail.com>
2024-08-15 01:53:24 +0100

    Comportamento dos menus

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ window.addEventListener('scroll', function() {
     var menu = document.querySelector('.header_menu');
     var menu_afastado = document.querySelector(".menu_afastado a");
 
-
     if (window.scrollY > header.clientHeight) {
         menu.classList.add('menu_scroll');
         menu_afastado.classList.add("menu_scroll");
@@ -15,10 +14,8 @@ window.addEventListener('scroll', function() {
 
 
 // - Menu escndido
-let menu_escondido = document.querySelector(".menu_escondido")
-const esc_mennu = document.querySelectorAll(".escoder")
+var menu_escondido = document.querySelector(".menu_escondido")
 let bx_menu = document.querySelector(".bx-menu")
-
 bx_menu.addEventListener("click", function(){
     menu_escondido.classList.toggle("menu_ativo")
     bx_menu.classList.toggle("bx_color")
@@ -26,4 +23,22 @@ bx_menu.addEventListener("click", function(){
     
 })
 
+document.addEventListener("DOMContentLoaded", function() {
+    function fecharMenu() {
+    
+        const esconder = document.querySelectorAll(".esconder");
+    
+        if (esconder.length > 0) {
+            esconder.forEach(function(element) {
+                element.addEventListener("click", function() {
+                 menu_escondido.classList.remove("menu_ativo")
+                });
+            });
+        } else {
+            console.log(" Sempre a erra, Mauro !!");
+        }
+    }
 
+   
+    fecharMenu();
+});
